67c393af27657526e8aad52ea7f6ce6516840db1
Carlos <lacerda.carlos@gmail.com>
2021-06-30 17:59:05 +0100

    Revert "add"
    This reverts commit 921314af

--- spring5-recipe-app/src/main/java/com/cmassystems/spring5recipeapp/services/RecipeService.java
+++
@@ -1,9 +0,0 @@
-package com.cmassystems.spring5recipeapp.services;
-
-import com.cmassystems.spring5recipeapp.domain.Recipe;
-
-import java.util.Set;
-
-public interface RecipeService {
-    Set<Recipe> getRecipes();
-}
